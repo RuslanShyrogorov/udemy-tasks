f45aaa4f1d6aefebf0a760379574e9f94619ef5c
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-08-21 21:36:04 +0300

    first task

---
+++ index.js
@@ -0,0 +1,21 @@
+// const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: { },
+  actors: { },
+  genres: [],
+  privat: false,
+}
+
+// const a = prompt('Последний прсмотренный фильм?', ''),
+//       b = +prompt('Оцените его', ''),
+//       c = prompt('Последний прсмотренный фильм?', ''),
+//       d = +prompt('Оцените его', '');
+
+
+personalMovieDB.movies[a] = b;
+
+personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
